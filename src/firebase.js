5039b6ae08d7997b16fe03ccac4f90067f6d7654
kyrawny <kyrawny@gmail.com>
2023-07-19 17:41:21 +0800

    changed api key

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 const firebaseConfig = {
-  apiKey: "AIzaSyCe8-IJKGUd-yXoYR0xQKMdSuIv5YsznDE",
-  authDomain: "fileuploads-be0db.firebaseapp.com",
-  projectId: "fileuploads-be0db",
-  storageBucket: "fileuploads-be0db.appspot.com",
-  messagingSenderId: "832032829361",
-  appId: "1:832032829361:web:f2079f619d4aad32ae9790",
+  apiKey: "AIzaSyBhcs89gkGUNCb6S_VxIYtfeiIidoL82uA",
+  authDomain: "deja-vu-9a3fd.firebaseapp.com",
+  projectId: "deja-vu-9a3fd",
+  storageBucket: "deja-vu-9a3fd.appspot.com",
+  messagingSenderId: "1055026215636",
+  appId: "1:1055026215636:web:778ce921dfea1e28729941"
 };
 
 // Initialize Firebase
